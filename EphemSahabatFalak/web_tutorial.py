58c4c96307c34f1dead8848706b33b7a407efe6c
Muhammad 'Izzat Mohd Zubir <muhammadizzatmohdzubir@Muhammads-MacBook-Air.local>
2024-06-26 00:06:37 +0800

    removed login page

--- EphemSahabatFalak/web_tutorial.py
+++ EphemSahabatFalak/web_tutorial.py
@@ -439,16 +439,5 @@ def tentangkami():
     return render_template("tentangkami.html")
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
